6858d71833de88a6ecee9dec04d8c11174d1b465
MorickClive <MorickClive@Gmail.com>
2021-01-26 10:48:42 +0000

    revert merge conflict

--- src/test/java/com/qa/selenium/DemoSite.java
+++ src/test/java/com/qa/selenium/DemoSite.java
@@ -13,57 +13,89 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.PageFactory;
-
-import com.qa.selenium.webpages.demosite.DemoSiteHomePage;
-import com.qa.selenium.webpages.demosite.pages.DemoAddUsersPage;
 
 public class DemoSite {
 
-	private static WebDriver driver;
-	private static WebElement targ;
-	private static Logger LOGGER = Logger.getGlobal();
+    private static WebDriver driver;
+    private static WebElement targ;
+    private static Logger LOGGER = Logger.getGlobal();
 
-	@BeforeClass
-	public static void initialise() {
+    @BeforeClass
+    public static void initialise(){
+    	
+        LOGGER.setLevel(Level.ALL);
+        
+        System.setProperty(
+        		"webdriver.chrome.driver",
+        		"src/test/resources/drivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        
+        // timeouts
+        driver.manage().timeouts().pageLoadTimeout(2, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+    }
 
-		LOGGER.setLevel(Level.ALL);
+    @Test
+    public void createUser(){
+        LOGGER.warning("Connecting to The Demo Site....");
 
-		System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
-		driver = new ChromeDriver();
+        // STAGE 1 - navigate to site.
+        // ========================================
+            // I want to navigate to....
+        	driver.get("http://thedemosite.co.uk/");
+        	
+        // STAGE 2 - create a user.
+        // ========================================
+            LOGGER.info("Creating a new user...\n");
+            // Navigate to add user page
+            targ = driver.findElement(By.xpath("/html/body/div/center/table/tbody/tr[2]/td/div/center/table/tbody/tr/td[2]/p/small/a[3]"));
+            targ.click();
+            
+            // Fill details out
+        	targ.findElement(By.name("username"));
+        	targ.sendKeys("root");
+        	targ.findElement(By.name("password"));
+        	targ.sendKeys("root");
+        	
+        	// Submit user
+        	targ.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/div/center/table/tbody/tr[3]/td[2]/p/input"));
+        	targ.click();
 
-		// timeouts
-		driver.manage().timeouts().pageLoadTimeout(2, TimeUnit.SECONDS);
-		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-	}
+        // STAGE 3 - Log in as created user.
+        // ========================================
+            LOGGER.info("Logging in as created user...\n");        	
+            // Navigate to login
+            targ = driver.findElement(By.xpath("/html/body/div/center/table/tbody/tr[2]/td/div/center/table/tbody/tr/td[2]/p/small/a[4]"));
+            targ.click();
+            
+        	targ.findElement(By.name("username"));
+        	targ.sendKeys("root");
+        	targ.findElement(By.name("password"));
+        	targ.sendKeys("root");
+        	
+        	// Login user
+        	targ.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/div/center/table/tbody/tr[3]/td[2]/p/input"));
+        	targ.click();
+        	
+        // STAGE 4 - check success.
+        // ========================================
+            LOGGER.info("Checking success of automated test...\n");
+            
+            targ.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/big/blockquote/blockquote/font/center/b"));
 
-	@Test
-	public void createUser() {
-		LOGGER.warning("Connecting to The Demo Site....");
+            String status = targ.getText();
+             
+            // Assert success
+            assertEquals("**Successful Login**", status);
+    }
 
-		DemoSiteHomePage website = PageFactory.initElements(driver, DemoSiteHomePage.class);
 
-		LOGGER.info("Creating a new user...\n");
-		website.navAddUser();
-		website.addUsers_Page.signUp("root", "root");
-		
-		LOGGER.info("Logging in as created user...\n");
-		website.navLogin();
-		website.login_Page.login("root", "root");
+    @AfterClass
+    public static void tearDown() {
+        LOGGER.warning("Closing webdriver instance!");
 
-		LOGGER.info("Checking success of automated test...\n");
-		String status = website.login_Page.getStatus();
+        driver.quit();
 
-		// Assert success
-		assertEquals("**Successful Login**", status);
-	}
-
-	@AfterClass
-	public static void tearDown() {
-		LOGGER.warning("Closing webdriver instance!");
-
-		driver.quit();
-
-		LOGGER.info("!!! Webdriver closed successfully !!!");
-	}
+        LOGGER.info("!!! Webdriver closed successfully !!!");
+    }
 }
