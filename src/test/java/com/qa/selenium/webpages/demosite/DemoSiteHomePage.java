e0df9f2f4bc796fda16e4a8a135c4af7ec08b3bb
MorickClive <MorickClive@Gmail.com>
2021-01-26 10:55:52 +0000

    AD: updating imports

--- src/test/java/com/qa/selenium/webpages/demosite/DemoSiteHomePage.java
+++ src/test/java/com/qa/selenium/webpages/demosite/DemoSiteHomePage.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.PageFactory;
 
 import com.qa.selenium.webpages.demosite.pages.DemoAddUsersPage;
 import com.qa.selenium.webpages.demosite.pages.DemoLoginPage;
